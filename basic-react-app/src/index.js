e4c3c347e297c55baa0deaf46d3fa11bdf37c42a
raopg <prateek.rao.08@gmail.com>
2019-08-28 14:35:17 -0700

    Playing around with JSX syntax

--- basic-react-app/src/index.js
+++ basic-react-app/src/index.js
@@ -5,8 +5,19 @@ import ReactDOM from 'react-dom'
 //const React =  require('react')
 //import statement is ES2015. Require is a CommonJS import statement
 //Create React component
+
+
 const App = function() {
-    return <div>Hi There!</div>
+    const buttonText = 'Click Me!'
+    return (
+        <div>
+            <label className="label" for="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText} {/* This is how we can reference JS variables in JSX */}
+            </button> {/*JSX requires double quotes for components. Also comments in render block need to be put in braces*/}
+        </div>
+    );
 }
 // OR 
 
